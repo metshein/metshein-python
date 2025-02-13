46fc271fdc9164884278e664031d98e4bd61a77b
metshein <metshein@gmail.com>
2025-02-13 10:09:28 +0200

    zf

---
+++ h16.py
@@ -0,0 +1,26 @@
+import os
+from datetime import date
+
+print(f"Tere {os.getlogin()}")
+print(f"Sinu rada {os.getcwd()}")
+
+kataloogidearv = 10
+kustuta = 8
+kp = str(date.today())
+
+try:
+    os.mkdir(kp)
+    print(f"{kp} kataloog loodud")
+    for i in range(1,kataloogidearv+1):
+        kaust = kp+"/"+str(i)
+        os.makedirs(kaust)
+except:
+    print("Kataloog juba olemas")
+
+
+# Kustutab kausta
+if os.path.exists(kp+"/"+str(kustuta)):
+    os.rmdir(kp+"/"+str(kustuta))
+    print(f"{kustuta} kataloog kustutatud")
+else:
+    print(f"{kustuta} kataloogi ei leitud")
