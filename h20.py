4f346e1c6f8b73adae882eee192b166f65764230
metshein <metshein@gmail.com>
2025-02-14 09:54:46 +0200

    drfg

---
+++ h20.py
@@ -0,0 +1,20 @@
+import requests
+
+# API võtme ja linna nime määramine
+city = "Haapsalu"
+api_key = "f4718da5798ba9aea22497d29515b83a"
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+# API päringu tegemine
+response = requests.get(url)
+
+# Vastuse kontrollimine
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+    weather = data['weather'][0]['description']
+    temperature = data['main']['temp']
+    print(f"Ilma kirjeldus: {weather}")
+    print(f"Temperatuur: {temperature} °C")
+else:
+    print("Viga andmete allalaadimisel:", response.status_code)
